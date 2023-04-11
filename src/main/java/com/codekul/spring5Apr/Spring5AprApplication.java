0996f08b49ae535ef1b0ce2edf7e14d350f43542
Madhuri Tayade <mt202202@appystack.com>
2023-04-11 18:14:23 +0530

    initial commit

--- src/main/java/com/codekul/spring5Apr/Spring5AprApplication.java
+++ src/main/java/com/codekul/spring5Apr/Spring5AprApplication.java
@@ -1,8 +1,11 @@
 package com.codekul.spring5Apr;
 
+import com.codekul.spring5Apr.beanscope.Car;
+import com.codekul.spring5Apr.beanscope.Vehicle;
 import com.codekul.spring5Apr.config.AppConfig;
 import com.codekul.spring5Apr.ioc.StudentImpl;
 import com.codekul.spring5Apr.ioc.SchoolService;
+import com.codekul.spring5Apr.ioc.TeacherImpl;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -14,7 +17,25 @@ public class Spring5AprApplication {
 		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
 		SchoolService schoolService = context.getBean(StudentImpl.class);
 		schoolService.doWork();
+		schoolService = context.getBean(TeacherImpl.class);
+		schoolService.doWork();
 
+		Vehicle v1 = context.getBean(Vehicle.class);
+		System.out.println("V1: "+v1.hashCode());
+		Vehicle v2 = context.getBean(Vehicle.class);
+		System.out.println("V2: "+v2.hashCode());
+		Vehicle v3 = context.getBean(Vehicle.class);
+		System.out.println("V3: "+v3.hashCode());
+
+		Car c1 = context.getBean(Car.class);
+		System.out.println("C1: "+c1.hashCode());
+		Car c2 = context.getBean(Car.class);
+		System.out.println("C2: "+c2.hashCode());
+
+		Car c3 = context.getBean(Car.class);
+		System.out.println("C3: "+c3.hashCode());
+
+		context.close();
 		//SpringApplication.run(Spring5AprApplication.class, args);
 	}
 
--- src/main/java/com/codekul/spring5Apr/config/AppConfig.java
+++ src/main/java/com/codekul/spring5Apr/config/AppConfig.java
@@ -1,10 +1,25 @@
 package com.codekul.spring5Apr.config;
 
+import com.codekul.spring5Apr.beanscope.Car;
+import com.codekul.spring5Apr.beanscope.Vehicle;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 @Configuration
 @ComponentScan("com.codekul.spring5Apr.ioc")
 public class AppConfig {
 
+    @Bean
+    //@Scope(value = "singleton")
+    public Vehicle getVehicle(){
+        return new Vehicle();
+    }
+
+    @Bean
+    @Scope(value = "prototype")
+    public Car getCar(){
+        return new Car();
+    }
 }
