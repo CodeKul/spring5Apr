4ef897a5770470fdcba76b9a2d08aa24ae6f80b2
Madhuri Tayade <mt202202@appystack.com>
2023-04-18 17:58:39 +0530

    spring boot introduction

--- src/main/java/com/codekul/spring5Apr/Spring5AprApplication.java
+++ src/main/java/com/codekul/spring5Apr/Spring5AprApplication.java
@@ -10,12 +10,20 @@ import com.codekul.spring5Apr.ioc.TeacherImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class Spring5AprApplication {
 
-	public static void main(String[] args) {
 
+	@GetMapping
+	public String hello(){
+		return "Hello World!!";
+	}
+
+	public static void main(String[] args) {
 		SpringApplication.run(Spring5AprApplication.class, args);
 
 		//used to create an object for application context
