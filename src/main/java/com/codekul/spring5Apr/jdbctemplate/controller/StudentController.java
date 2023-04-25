69568ea5af8e603b0fa1b108d05e87dc056b5023
Madhuri Tayade <mt202202@appystack.com>
2023-04-25 17:59:40 +0530

    JDBC template Post MEthod

--- src/main/java/com/codekul/spring5Apr/jdbctemplate/controller/StudentController.java
+++ src/main/java/com/codekul/spring5Apr/jdbctemplate/controller/StudentController.java
@@ -1,11 +1,12 @@
 package com.codekul.spring5Apr.jdbctemplate.controller;
 
+import com.codekul.spring5Apr.jdbctemplate.entity.Student;
 import com.codekul.spring5Apr.jdbctemplate.service.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/student")
 public class StudentController {
     @Autowired
     private StudentRepository studentRepository;
@@ -13,5 +14,10 @@ public class StudentController {
     @GetMapping("/getStudentCount")
     public Integer getStudentCount(){
           return studentRepository.getStudentCount();
+    }
+
+    @PostMapping("/saveStudent")
+    public String saveStudent(@RequestBody Student student){
+        return studentRepository.saveStudent(student);
     }
 }
--- src/main/java/com/codekul/spring5Apr/jdbctemplate/service/StudentRepository.java
+++ src/main/java/com/codekul/spring5Apr/jdbctemplate/service/StudentRepository.java
@@ -1,8 +1,22 @@
 package com.codekul.spring5Apr.jdbctemplate.service;
 
+import com.codekul.spring5Apr.jdbctemplate.entity.Student;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface StudentRepository {
     Integer getStudentCount();
+
+    /**
+     *
+     * {
+     *     "rno":12,
+     *     "name":"Anuja",
+     *     "age":26,
+     *     "city":"Pune",
+     *     "email":"anuja@gmail.com",
+     *     "phone":"896522"
+     * }
+     */
+    String saveStudent(Student student);
 }
